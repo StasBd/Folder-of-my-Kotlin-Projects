1256f78d5dc5e9c27e7026a57ddab616d24bced0
Stanislav <129835954+StasBd@users.noreply.github.com>
2023-09-02 19:56:09 +0300

    Main function which will add numbers!

---
+++ Function-To-Add-Numbers/src/main/kotlin/FunAddNums.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val FirstNumber = 15 //Any number here!
+    val SecondNumber = 25 //There too any number!
+    var Summary = FirstNumber + SecondNumber
+    println("We calculated your numbers and it will be: $Summary")
+}
