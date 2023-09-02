b6a199e4f6fcad3b975b1c3a6f5fc0c650fd7c84
Stanislav <129835954+StasBd@users.noreply.github.com>
2023-09-02 19:46:42 +0300

    If, Else and some comparing

---
+++ Condtional-Logic-Example/src/main/kotlin/ConditionalLogicExample.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // conditional logic example!
+    var a = 10
+    if (a < 15) {
+        println("A is smaller then 15")
+    } else  {
+        println("A is bigger then 15")
+    }
+}
