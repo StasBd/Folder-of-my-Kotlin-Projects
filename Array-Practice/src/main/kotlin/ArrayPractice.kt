93c3c922363c76d6765de8c6c1b50e4a1afd5cba
Stanislav <129835954+StasBd@users.noreply.github.com>
2023-09-02 22:53:32 +0300

    Array practice

---
+++ Array-Practice/src/main/kotlin/ArrayPractice.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //So we created array/massive
+var numbers = arrayOf(165, 350, 151, 177, 644)
+    // we can change element by using square brackets
+    numbers[3] = 160
+    // or can print element by using println and squares
+    println(numbers[0])
+    // or work with cycle
+    numbers.forEach { payextra -> if (165 > 7)
+        println("There number is so big!!!")
+    }
+}
